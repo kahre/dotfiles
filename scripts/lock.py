128e66876436f894d4fef1f276abeae2496f0c11
Jonthan Kåhre <Jonte.k_1993@hotmail.com>
2018-10-23 19:28:35 +0200

    Update lock script

--- scripts/lock.py
+++ scripts/lock.py
@@ -1,14 +1,56 @@
 #!/usr/bin/python3
-from background import FindImagePaths
-from background import randomImagePath
 import subprocess
+import os, random
 
-ls = FindImagePaths(True, False, False)
+directory = os.environ["HOME"] + "Pictures/Wallpaper"
 
-path = randomImagePath(ls)
-found = True
-# Black and White
-command = "i3lock --textcolor=ffffff00 --insidecolor=ffffff1c --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -i " + str(path)
-# With colour
-#command = "i3lock --insidevercolor=00000000 --insidewrongcolor=00000000 --insidecolor=00000000 --ringvercolor=faf611ff --ringwrongcolor=ff0000ff --ringcolor=40409000 --textcolor=ffffff00 --linecolor=aaaaaaff --keyhlcolor=009dffff --bshlcolor=ff8000ff -i " + str(path)
-subprocess.call(command.split())
+image = random.choice(os.listdir(directory))
+path = directory + "/" + image
+
+print("Loading image: " + path)
+
+# Colours 
+insidecolor = "ffffff1c"
+ringcolor = "ffffff3e"
+linecolor = "ffffff00"
+keyhlcolor = "00000080"
+ringvercolor = "00000000"
+insidevercolor = "0000001c"
+ringwrongcolor = "00000055"
+insidewrongcolor = "0000001c"
+timecolor = "afafafff"
+datecolor = "a5a5a5ff"
+
+command = ["i3lock"]
+
+command.append("--screen=1")
+command.append("--ignore-empty-password")
+command.append("--insidecolor=" + insidecolor)
+command.append("--ringcolor=" + ringcolor)
+command.append("--linecolor=" + linecolor)
+command.append("--keyhlcolor=" + keyhlcolor)
+command.append("--ringvercolor=" + ringvercolor)
+command.append("--insidevercolor=" + insidevercolor)
+command.append("--ringwrongcolor=" + ringwrongcolor)
+command.append("--insidewrongcolor=" + insidewrongcolor)
+command.append("--blur=4")
+command.append("--clock")
+command.append("--timecolor=" + timecolor)
+command.append("--timepos=3000:150")
+command.append("--timesize=50")
+command.append("--datecolor=" + datecolor)
+command.append("--verifcolor=" + timecolor)
+command.append("--wrongcolor=" + timecolor)
+command.append("--verif-font=fontawesome")
+command.append("--wrong-font=fontawesome")
+command.append("--verifsize=50")
+command.append("--wrongsize=50")
+command.append("--veriftext=")
+command.append("--wrongtext=")
+command.append("--noinputtext=")
+
+#command.append("--image=" + str(path))
+
+print(command)
+
+subprocess.call(command)
