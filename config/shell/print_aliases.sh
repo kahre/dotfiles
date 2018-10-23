cee9463e082b2035ff0b28918f9841d02e16243a
Jonthan Kåhre <Jonte.k_1993@hotmail.com>
2018-10-23 20:07:41 +0200

    minor alias print update

--- config/shell/print_aliases.sh
+++ config/shell/print_aliases.sh
@@ -16,22 +16,22 @@ CM_RESET='printf '${CL_RESET};
 
 SD_COLORIZE_ALIAS="s/alias \([^=]*\)=\(.*\)/`$CM_CYAN`\1`$CM_RESET` -> `$CM_PURP`\2`$CM_RESET`/g"
 SD_COLORIZE_SECTION="s/#.*/`$CM_BLUE`\0/g"
+SD_REMOVE_REDUNDANT="/^$/d; s/#/\n#/g"
 
 if [ $# -eq 0 ]; then
-  if [ `command -v bat` != "" ]; then
-    cat ${ALIASPATH} | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d" | bat
-  else
-    cat ${ALIASPATH} | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d" | less 
-  fi
+    if [ `command -v bat` != "" ]; then
+        cat ${ALIASPATH} | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d" | sed "${SD_REMOVE_REDUNDANT}" | bat
+    else
+        cat ${ALIASPATH} | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d" | sed "${SD_REMOVE_REDUNDANT}" | less
+    fi
 else
-  if [ $1 == "sections" ]; then 
-    cat ${ALIASPATH} | sed "${SD_COLORIZE_SECTION}" | sed "/#/!d"
-    exit
-  fi
-  printf "\n${CL_RED}Section matching \"$1\" for `whoami`\n"
-  cat ${ALIASPATH} | sed -n -e "/# $1/,/#/ p" | sed -e '2d;$d' | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d"
-  printf "\n${CL_RED}Aliases containing \"$1\" for `whoami`\n"
-  cat ${ALIASPATH} | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d" | sed "/$1/!d" | sed "/#/d"
+    if [ $1 == "sections" ]; then
+        cat ${ALIASPATH} | sed "${SD_COLORIZE_SECTION}" | sed "${SD_REMOVE_REDUNDANT}" | sed "/#/!d"
+        exit
+    fi
+    printf "\n${CL_RED}Section matching \"$1\" for `whoami`\n"
+    cat ${ALIASPATH} | sed -n -e "/# $1/,/#/ p" | sed -e '2d;$d' | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "${SD_REMOVE_REDUNDANT}" | sed "/pals/d"
+    printf "\n${CL_RED}Aliases containing \"$1\" for `whoami`\n"
+    cat ${ALIASPATH} | sed "${SD_COLORIZE_ALIAS}" | sed "${SD_COLORIZE_SECTION}" | sed "/pals/d" | sed "/$1/!d" | sed "/#/d" | sed "${SD_REMOVE_REDUNDANT}"
 fi
 printf "\n"
-
