15dc62319ff364c258b153b106de26761d9e9390
Jonthan Kåhre <Jonte.k_1993@hotmail.com>
2018-12-29 22:55:11 +0100

    Updates

---
+++ scripts/disable_screensaver.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+xset s off
+xset -dpms
+xset r rate 400 60
---
+++ scripts/setup_screens.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+xrandr --output HDMI-2 --primary --auto --output DVI-I-1-1 --mode 1280x1024 -r 76.11 --left-of HDMI-2
