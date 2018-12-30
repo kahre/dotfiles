b944ec43f1594088da9fc7dd747cd6ff55838029
kahre <none of your business>
2018-12-30 19:57:19 +0100

    add laptop settings

--- scripts/setup_screens.sh
+++ scripts/setup_screens.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-xrandr --output HDMI-2 --primary --auto --output DVI-I-1-1 --mode 1280x1024 -r 76.11 --left-of HDMI-2
+#xrandr --output HDMI-2 --primary --auto --output DVI-I-1-1 --mode 1280x1024 -r 76.11 --left-of HDMI-2
+xrandr --output eDP1 --primary --auto
