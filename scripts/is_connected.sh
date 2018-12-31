dec9ee28244995728b7b2a3930bf142c43f5b1ed
Jonthan Kåhre <Jonte.k_1993@hotmail.com>
2018-12-31 18:56:46 +0100

    improve script to check internet connectivity

--- scripts/is_connected.sh
+++ scripts/is_connected.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-res=`ping duckduckgo.com -c 1 -q -W 2 -w 2 | grep 'trans' | wc -l`
-#echo ">>> $res"
-if [ "$res" -eq "1" ]
-then exit
-else exit -1
+wget -q --spider https://duckduckgo.com
+
+if [ $? -eq 0 ]; then 
+    exit
+else 
+    exit -1
 fi
